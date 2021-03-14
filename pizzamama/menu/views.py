cefe922ffd3dc9c2ebe0b87da2ca7b2003d1e296
Fanch DANIEL <fanch.daniel@gmail.com>
2021-03-14 12:27:44 +0100

    app main

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -7,17 +7,17 @@ from .models import Pizza
 # Create your views here.
 
 
+
+"""            
+            trier par prix
+                .order_by
+
+"""
+
+
+
 # / menu
 def index(request):
-    pizzas = Pizza.objects.all()
-    return render(request, "menu/index.html" , {'pizzas' : pizzas})
+    pizzas = Pizza.objects.all().order_by('prix')
+    return render(request, "menu/index.html", {'pizzas': pizzas})
 
-
-"""
-def index(request):
-    pizzas = Pizza.objects.all()
-    pizzas_names_and_prices = [pizza.nom + " : " + str(pizza.prix) + "€" for pizza in pizzas]
-    pizzas_names_and_prices_str = ", " .join(pizzas_names_and_prices)
-    return HttpResponse("Les Pizzas : " + pizzas_names_and_prices_str)
-
-"""
